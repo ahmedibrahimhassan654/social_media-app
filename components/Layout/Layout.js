9d289cb102bc9af7513a43e773a1535f49f94b70
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-01-29 22:58:26 +0200

    9. App.js, Layout and Navbar

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -2,8 +2,22 @@ import React from "react";
 import HeadTags from "./HeadTags";
 import Navbar from "./Navbar";
 import { Container } from "semantic-ui-react";
+import Router from "next/router";
+import nProgress from "nprogress";
 
 function Layout({ children }) {
+  Router.onRouteChangeStart = () => {
+    nProgress.start();
+  };
+
+  Router.onRouteChangeComplete = () => {
+    nProgress.done();
+  };
+
+  Router.onRouteChangeError = () => {
+    nProgress.done();
+  };
+
   return (
     <>
       <HeadTags />
--- components/Layout/Navbar.js
+++ components/Layout/Navbar.js
@@ -1,13 +1,38 @@
 import React from "react";
-import { Container, Menu } from "semantic-ui-react";
-
+import { Container, Menu, Icon } from "semantic-ui-react";
+import { useRouter } from "next/router";
+import Link from "next/link";
 function Navbar() {
+  const router = useRouter();
+  const isActive = (route) => router.pathname === route;
+  // console.log(router.pathname);
   return (
-    <Menu fluid>
-      <Container fluid>
-        <Link></Link>
-      </Container>
-    </Menu>
+    <>
+      <Menu fluid>
+        <Container text>
+          <Link href="/login">
+            <Menu.Item header active={isActive("/login")}>
+              <Icon
+                size="large"
+                color={isActive("/login") ? " teal" : "red"}
+                name="sign in"
+              />
+              Login
+            </Menu.Item>
+          </Link>
+          <Link href="/signup">
+            <Menu.Item header active={isActive("/signup")}>
+              <Icon
+                size="large"
+                color={isActive("/signup") ? " teal" : "red"}
+                name="signup"
+              />
+              Sign Up
+            </Menu.Item>
+          </Link>
+        </Container>
+      </Menu>
+    </>
   );
 }
 
