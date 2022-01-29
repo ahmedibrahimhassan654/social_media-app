626868cdf83aded2d524eb7cf0bf317e14908b58
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-01-29 20:51:29 +0200

    creating express server

---
+++ components/Layout/Navbar.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Container, Menu } from "semantic-ui-react";
+
+function Navbar() {
+  return (
+    <Menu fluid>
+      <Container fluid>
+        <Link></Link>
+      </Container>
+    </Menu>
+  );
+}
+
+export default Navbar;
