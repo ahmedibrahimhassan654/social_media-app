2c341f9e60d3f7ec201affb7bb3ec2d2c906fcba
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-01-31 13:53:01 +0200

    structure user inputs and mange states

--- pages/signup.js
+++ pages/signup.js
@@ -1,13 +1,118 @@
-import React from "react";
+import React, { useState, useEffect, useRef } from "react";
 import {
   FooterMessage,
   HeaderMessage,
 } from "../components/common/WelcomeMessage";
+import { Form, Button, Message, Segment, Divider } from "semantic-ui-react";
 const signup = () => {
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    bio: "",
+    facebook: "",
+    youtube: "",
+    twitter: "",
+    instagram: "",
+  });
+
+  const { name, email, password, bio } = user;
+  const regexUserName = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/;
+
+  const [showSocialLinks, setShowSocialLinks] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [formLoading, setFormLoading] = useState(false);
+  const [username, setUsername] = useState("");
+  const [usernameLoading, setUsernameLoading] = useState(false);
+  const [usernameAvailable, setUsernameAvailable] = useState(false);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
   return (
     <>
       <HeaderMessage />
+      <Form
+        loading={formLoading}
+        error={errorMsg !== null}
+        onSubmit={handleSubmit}
+      >
+        <Message
+          error
+          header="Oops!"
+          content={errorMsg}
+          onDismiss={() => setErrorMsg(null)}
+        />
 
+        <Segment>
+          <Form.Input
+            required
+            label="Name"
+            placeholder="Name"
+            name="name"
+            value={name}
+            onChange={handleChange}
+            fluid
+            icon="user"
+            iconPosition="left"
+          />
+
+          <Form.Input
+            required
+            label="Email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            type="email"
+          />
+          <Form.Input
+            label="Password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            icon={{
+              name: "eye",
+              circular: true,
+              link: true,
+              onClick: () => setShowPassword(!showPassword),
+            }}
+            iconPosition="left"
+            type={showPassword ? "text" : "password"}
+            required
+          />
+          <Form.Input
+            loading={usernameLoading}
+            error={!usernameAvailable}
+            required
+            label="Username"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => {
+              setUsernameLoading(true);
+              setUsername(e.target.value);
+
+              if (regexUserName.test(e.target.value)) {
+                setUsernameAvailable(true);
+                setUsernameLoading(false);
+              } else {
+                setUsernameAvailable(false);
+              }
+            }}
+            fluid
+            icon={usernameAvailable ? "check" : "close"}
+            iconPosition="left"
+          />
+        </Segment>
+      </Form>
       <FooterMessage />
     </>
   );
