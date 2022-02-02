b590e00a70197a878cf76230196a9535e680b71f
Ahmed  Ibrahim <ahmedibrahimhassan654@gmail.com>
2022-02-02 19:16:05 +0200

    finished signup and login component

--- pages/login.js
+++ pages/login.js
@@ -1,14 +1,93 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { Form, Button, Message, Segment, Divider } from "semantic-ui-react";
 import {
   FooterMessage,
   HeaderMessage,
 } from "../components/common/WelcomeMessage";
 
 const login = () => {
+  const [user, setUser] = useState({
+    email: "",
+    password: "",
+  });
+  const { email, password } = user;
+  const [showPassword, setShowPassword] = useState(false);
+  const [errorMsg, setErrorMsg] = useState(null);
+  const [formLoading, setFormLoading] = useState(false);
+  const [submitDisabled, setSubmitDisabled] = useState(true);
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setUser((prev) => ({ ...prev, [name]: value }));
+  };
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    console.log(e);
+  };
+  useEffect(() => {
+    const isUser = Object.values({ email, password }).every((item) =>
+      Boolean(item)
+    );
+    isUser ? setSubmitDisabled(false) : setSubmitDisabled(true);
+  }, [user]);
   return (
     <>
       <HeaderMessage />
+      <Form
+        loading={formLoading}
+        error={errorMsg !== null}
+        onSubmit={handleSubmit}
+      >
+        <Message
+          error
+          header="Oops!"
+          content={errorMsg}
+          onDismiss={() => setErrorMsg(null)}
+        />
 
+        <Segment>
+          <Form.Input
+            required
+            label="Email"
+            placeholder="Email"
+            name="email"
+            value={email}
+            onChange={handleChange}
+            fluid
+            icon="envelope"
+            iconPosition="left"
+            type="email"
+          />
+
+          <Form.Input
+            label="Password"
+            placeholder="Password"
+            name="password"
+            value={password}
+            onChange={handleChange}
+            fluid
+            icon={{
+              name: "eye",
+              circular: true,
+              link: true,
+              onClick: () => setShowPassword(!showPassword),
+            }}
+            iconPosition="left"
+            type={showPassword ? "text" : "password"}
+            required
+          />
+
+          <Divider hidden />
+          <Button
+            icon="signup"
+            content="Login"
+            type="submit"
+            color="orange"
+            disabled={submitDisabled}
+          />
+        </Segment>
+      </Form>
       <FooterMessage />
     </>
   );
